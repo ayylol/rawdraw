f2c4df60530abdb36c539f057c03aac683b2af73
David Garcia Orozco <David.GarOro@protonmail.com>
2026-08-21 16:50:57 -0600

    Broken rn... halfway through adding ncurses support with newer changes

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <locale.h>
+#include <ncurses.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,14 +8,14 @@
 
 #include "linalg.h"
 #include "rawdraw.h"
-#include "xwrapper.h"
 
 #define DRAW_TICK 0.0167
-#define WIDTH 1420
-#define HEIGHT 840
+#define WIDTH 100
+#define HEIGHT 50
 uint32_t buffer[WIDTH*HEIGHT];
 char file_name[64] = "out.ppm";
 
+void init_ncurses();
 int32_t save_ppm(char* file_name, uint32_t *buffer, 
                   uint32_t width, uint32_t height);
 void draw_frame(image_t img);
@@ -26,15 +28,48 @@ int32_t main(int argc, char* argv[]) {
     strcpy(file_name, argv[1]); 
   }
   image_t img = {.buffer=buffer, .w=WIDTH, .h=HEIGHT};
-
-  XWrapper x_ctx;
-  X11Init(&x_ctx);
-  X11BindBuffer(&x_ctx, img.buffer, WIDTH, HEIGHT);
-
   clock_t t=clock();
   double draw_t=DRAW_TICK;
-  Events events;
   
+  init_ncurses();
+  uint8_t ch = ' ';
+  while( ch != 'q' ) {
+    clock_t curr_t=clock();
+    double delta_t=(double)(curr_t-t)/CLOCKS_PER_SEC;
+    t=curr_t;
+    draw_t+=delta_t;
+    if (draw_t >= DRAW_TICK){
+      draw_frame(img);
+      draw_t=0.0;
+    }
+    for (int32_t i=0; i < img.w; i++){
+      for (int32_t j=0; j < img.h; j++){
+        ch='#';
+        //ch=L'\u28FF';
+        int8_t col_pair=1;
+        switch(img.buffer[rawdraw_get_i(img, i, j)]){
+          case X000_BLACK:  ch=' ';     break;
+          case X001_MAROON: col_pair=1; break;
+          case X002_GREEN:  col_pair=2; break;
+          case X003_OLIVE:  col_pair=3; break;
+          case X004_NAVY:   col_pair=4; break;
+          case X006_TEAL:   col_pair=5; break;
+          case X007_SILVER: col_pair=6; break;
+          case X008_GREY:   col_pair=7; break;
+        };
+        attron(COLOR_PAIR(col_pair));
+        if (ch == ' ') {
+          mvaddch(j, i, ch);
+        } else {
+          mvaddstr(j, i, "\xE2\xA3\xBF");
+        }
+      }
+    }
+    refresh();
+    save_ppm(file_name, img.buffer, img.w, img.h);
+    ch = getch();
+  }
+  /*
   while (true) {
     clock_t curr_t=clock();
     double delta_t=(double)(curr_t-t)/CLOCKS_PER_SEC;
@@ -44,12 +79,28 @@ int32_t main(int argc, char* argv[]) {
       draw_frame(img);
       draw_t=0.0;
     }
-    X11DrawFrame(&x_ctx);
-    X11PollEvents(&x_ctx, &events);
     if (events.exit) break;
   }
-  X11Destroy(&x_ctx);
+  */
   return 0;
+}
+
+void init_ncurses(){
+  initscr();
+  setlocale(LC_ALL, "C.utf8");
+  start_color();
+  cbreak();
+  keypad(stdscr, true);
+  noecho();
+  curs_set(0);
+  init_pair(1, COLOR_WHITE, COLOR_BLACK);
+  init_pair(2, COLOR_RED, COLOR_BLACK);
+  init_pair(3, COLOR_GREEN, COLOR_BLACK);
+  init_pair(4, COLOR_BLUE, COLOR_BLACK);
+  init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+  init_pair(6, COLOR_YELLOW, COLOR_BLACK);
+  init_pair(7, COLOR_CYAN, COLOR_BLACK);
+
 }
 
 int s=0;
@@ -95,7 +146,7 @@ void draw_frame(image_t img){
     //quad[i]=div_sv3(quad[i].z,quad[i]);
   }
   for (int i=0;i<23;i+=3){
-    rawdraw_tri(img, to_point(v3_to_v2(quad[i])),   to_point(v3_to_v2(quad[i+1])),  to_point(v3_to_v2(quad[i+2])),colors[2+(i/6)%6]);
+    rawdraw_tri(img, to_point(v3_to_v2(quad[i])),   to_point(v3_to_v2(quad[i+1])),  to_point(v3_to_v2(quad[i+2])),X007_SILVER);
   }
   s+=i16_PI/300;
   /*
